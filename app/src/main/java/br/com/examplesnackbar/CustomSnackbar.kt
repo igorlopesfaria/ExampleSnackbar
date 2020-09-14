4102fce278cb9c4f9e7d9ad27a75dfb660e49928
igor.faria <igo.faria@sympla.com.br>
2020-09-14 20:35:14 -0300

    first commit

---
+++ app/src/main/java/br/com/examplesnackbar/CustomSnackbar.kt
@@ -0,0 +1,63 @@
+package br.com.examplesnackbar
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.annotation.LayoutRes
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
+
+class CustomSnackbar {
+
+    companion object {
+        fun makeActionMessage(
+            parent: View,
+            iconImage: Int? = null,
+            messageText: String,
+            actionText: String? = null,
+            action: (() -> Unit)? = null,
+            duration: Int = Snackbar.LENGTH_INDEFINITE
+        ): Snackbar {
+            val snackbar = Snackbar.make(parent, "", duration)
+
+            val view = snackbar.addCustomView(R.layout.view_snackbar_simple)
+
+            val messageTextView: TextView = view.findViewById(R.id.messageTextView)
+            val iconIMG: ImageView = view.findViewById(R.id.iconIMG)
+            val actionTextView: TextView = view.findViewById(R.id.actionTextView)
+
+            iconImage?.let {
+                iconIMG.setImageResource(iconImage)
+            }?: run{
+                iconIMG.visibility = View.GONE
+            }
+
+            messageTextView.text = messageText
+
+            actionText?.let {
+                actionTextView.text = actionText
+                actionTextView.setOnClickListener { action?.invoke() }
+            }?: run{
+                actionTextView.visibility = View.GONE
+            }
+            return snackbar
+        }
+    }
+}
+
+fun Snackbar.addCustomView(@LayoutRes layout: Int): View {
+    val snackbarLayout = this.view as Snackbar.SnackbarLayout
+    val snackbarContentLayout: SnackbarContentLayout = snackbarLayout.getChildAt(0) as SnackbarContentLayout
+    val customView = LayoutInflater.from(this.context).inflate(layout, snackbarLayout, false)
+
+    snackbarContentLayout.findViewById<TextView>(R.id.snackbar_text).visibility = View.INVISIBLE
+    snackbarLayout.setPadding(0, 0, 0, 0)
+    snackbarLayout.setBackgroundColor(Color.TRANSPARENT)
+    if (snackbarContentLayout.getChildAt(0) is AppCompatTextView)
+        snackbarContentLayout.addView(customView, 0)
+    return customView
+}
---
+++ app/src/main/java/br/com/examplesnackbar/SnackbarActivity.kt
@@ -0,0 +1,97 @@
+package br.com.examplesnackbar
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+
+class SnackbarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_snackbar)
+
+        val simpleSnackbarBTN = findViewById<Button>(R.id.simpleSnackbarBTN)
+        val longSnackbarBTN = findViewById<Button>(R.id.longSnackbarBTN)
+        val indefiniteSnackbarBTN = findViewById<Button>(R.id.indefiniteSnackbarBTN)
+        val genericMethodSnackbarBTN = findViewById<Button>(R.id.genericMethodSnackbarBTN)
+        val addActionSnackbarBTN = findViewById<Button>(R.id.addActionSnackbarBTN)
+        val customSnackbarBTN = findViewById<Button>(R.id.customSnackbarBTN)
+        val callbackSnackbarBTN = findViewById<Button>(R.id.callbackSnackbarBTN)
+        val customViewSnackbarBTN = findViewById<Button>(R.id.customViewSnackbarBTN)
+
+        simpleSnackbarBTN.setOnClickListener {
+            Snackbar.make(it, "Simple Snackbar", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+
+        longSnackbarBTN.setOnClickListener {
+            Snackbar.make(it, "Long Snackbar", Snackbar.LENGTH_LONG)
+                .show()
+        }
+
+        genericMethodSnackbarBTN.setOnClickListener {
+            genericMethodSnackbar(it, "Generic Method Snackbar")
+        }
+
+        addActionSnackbarBTN.setOnClickListener {
+            Snackbar.make(it, "Snack with action show toast", Snackbar.LENGTH_LONG)
+                .setAction("Action") {
+                    Log.i("Snackbar", "Clicou na ação!")
+                }
+                .show()
+        }
+
+        indefiniteSnackbarBTN.setOnClickListener {
+            Snackbar.make(it, "Infinite Snackbar", Snackbar.LENGTH_INDEFINITE)
+                .show()
+        }
+
+        customSnackbarBTN.setOnClickListener {
+            Snackbar.make(it, "Custom snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action") {
+                    Log.i("Snackbar", "Clicou na ação!")
+                }
+                .setTextColor(ContextCompat.getColor(applicationContext, R.color.black))
+                .setBackgroundTint(ContextCompat.getColor(applicationContext, R.color.purple_200))
+                .setActionTextColor(ContextCompat.getColor(applicationContext, R.color.teal_700))
+                .show()
+        }
+
+
+        callbackSnackbarBTN.setOnClickListener {
+            Snackbar.make(it, "Infinite Snackbar", Snackbar.LENGTH_LONG)
+                .addCallback( object:Snackbar.Callback(){
+                    override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                        super.onDismissed(transientBottomBar, event)
+                        Log.i("Snackbar", "Dismissed")
+                    }
+
+                    override fun onShown(sb: Snackbar?) {
+                        super.onShown(sb)
+                        Log.i("Snackbar", "Shown")
+                    }
+                })
+                .show()
+        }
+
+        customViewSnackbarBTN.setOnClickListener {
+            CustomSnackbar.makeActionMessage(
+                parent = findViewById(android.R.id.content),
+                iconImage = android.R.drawable.ic_menu_close_clear_cancel,
+                messageText = "Custom view component",
+                actionText = "Action",
+                action = {
+                    Log.i("Snackbar", "Action click listener")
+                }
+            ).show()
+        }
+    }
+
+    private fun genericMethodSnackbar(view: View, text: String, duration: Int = Snackbar.LENGTH_SHORT){
+        Snackbar.make(view, text, duration).show()
+    }
+}
